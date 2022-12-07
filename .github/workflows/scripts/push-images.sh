5343ced66081a704be447199e549187cc9c85e05
Marco Franssen <marco.franssen@gmail.com>
2022-12-07 11:30:58 +0100

    Add push-images.sh docs
    Signed-off-by: Marco Franssen <marco.franssen@gmail.com>

--- .github/workflows/scripts/push-images.sh
+++ .github/workflows/scripts/push-images.sh
@@ -1,18 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck shell=bash
+##
+## USAGE: __PROG__
+##
+## "__PROG__" publishes images to a registry.
+##
+## Usage example(s):
+##   ./__PROG__ 1.5.2
+##   ./__PROG__ v1.5.2
+##   ./__PROG__ refs/tags/v1.5.2
+##
+## Commands
+## - ./__PROG__ <version>    pushes images to the registry using given version.
 
 set -e
 
-IMAGETAG="$1"
-if [ -z "$IMAGETAG" ]; then
-    echo "IMAGETAG not provided!" 1>&2
-    echo "Usage: push-image.sh IMAGETAG" 1>&2
+function usage {
+  grep '^##' "$0" | sed -e 's/^##//' -e "s/__PROG__/$me/" >&2
+}
+
+me=$(basename "$0")
+
+version="$1"
+# remove the git tag prefix
+# Push the images using the version tag (without the "v" prefix).
+# Also strips the refs/tags part if the GITHUB_REF variable is used.
+version="${version#refs/tags/v}"
+version="${version#v}"
+
+if [ -z "${version}" ]; then
+    usage
+    echo "version not provided!" 1>&2
     exit 1
 fi
 
-echo "Pushing image tagged as $IMAGETAG..."
+echo "Pushing image tagged as ${version}..."
 
 LOCALIMG=ghcr.io/spiffe/spiffe-csi-driver:devel
-REMOTEIMG=ghcr.io/spiffe/spiffe-csi-driver:"${IMAGETAG}"
+REMOTEIMG=ghcr.io/spiffe/spiffe-csi-driver:"${version}"
 
 echo "Executing: docker tag $LOCALIMG $REMOTEIMG"
 docker tag "$LOCALIMG" "$REMOTEIMG"
